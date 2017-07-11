19e3b25f322da1e069b69f0b48e568ba65711ba6
m.buils <m.buils@digital-home-concept.com>
2017-07-11 12:35:57 +0200

    [MBU] Add search wikipedia links exercice

--- src/search.ts
+++ src/search.ts
@@ -1,18 +1,18 @@
 const rpn = require('request-promise-native');
 
 exports.Search = class Search {
-    private urlSearch(query: string) {
+    private urlSearch(query: string): string {
         return `https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${query}`;
     }
 
-    private async search() {
+    private async search(): Promise<string[]> {
         const [, , , linksAngular] = JSON.parse(await rpn(this.urlSearch('angular')));
         const [, , , linksTypescript] = JSON.parse(await rpn(this.urlSearch('typescript')));
 
         return [...linksAngular, ...linksTypescript];
     }
 
-    async execute() {
+    async execute(): Promise<void> {
         const links = await this.search();
 
         console.log('liens: ', links);
